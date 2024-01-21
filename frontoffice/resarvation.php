4371190e506b69b24ec663127d873e91a93c2c8a
manassahoo2 <manassahoo380@gmail.com>
2024-01-21 21:47:21 +0530

    Inital commit

---
+++ frontoffice/resarvation.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resarvation</title>
+      <link href="../css/bootstrap.css" rel="stylesheet">
+      <link href="../css/jquery.smartmenus.bootstrap.css" rel="stylesheet">
+      <link id="switcher" href="../css/theme-color/default-theme.css" rel="stylesheet">
+      <link href="../css/style.css" rel="stylesheet">
+      <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+      <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+  <section id="menu">
+         <div class="container">
+            <div class="menu-area">
+               <?php include('../navbar.php') ?>
+            </div>
+        </div>
+    </section>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="js/bootstrap.js"></script>  
+      <script type="text/javascript" src="js/jquery.smartmenus.js"></script>
+      <script type="text/javascript" src="js/jquery.smartmenus.bootstrap.js"></script>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,61 @@
+
+               <!-- Navbar -->
+               <div class="navbar navbar-default" role="navigation">
+                  <div class="navbar-header">
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                     <span class="sr-only">Toggle navigation</span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     </button>
+                  </div>
+                  <div class="navbar-collapse collapse">
+
+                     <!-- Left nav -->
+                     <ul class="nav navbar-nav">
+                        <li>
+                        <!-- <img style="max-width: 150px;" src="#" alt="Control Panel"> -->
+                        </a>
+                        </li>
+
+                        <li>
+                           <a href="#"> <i class='bx bx-desktop'></i> Frontoffice <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="frontoffice/resarvation.php">Resarvation</a></li>
+                           </ul>
+                        </li>
+
+                        <li>
+                           <a href="#"> <i class='bx bxs-home'></i> Housekeeping <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="#">add</a></li> 
+                           </ul>
+                        </li>
+
+                        <li>
+                        <a href="#"> <i class='bx bx-restaurant'></i> Restaurant <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="#">add</a></li> 
+                           </ul>
+                        </li>
+
+                        <li>
+                           <a href="#"> <i class='bx bxs-calculator'></i> Accounts <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="#">add</a></li>
+                           </ul>
+                        </li>
+
+                        <li>
+                           <a href="#"> <i class='bx bxs-cart'></i> Store <span class="caret"></span></a>
+                           <ul class="dropdown-menu">
+                              <li><a href="#">add</a></li>
+                           </ul>
+                        </li>
+                        <li>
+                           <a href="#"> <i class='bx bx-cog'></i> Settings </a>
+                        </li>
+                     </ul>
+                  </div>
+                  <!--/.nav-collapse -->
+               </div>
